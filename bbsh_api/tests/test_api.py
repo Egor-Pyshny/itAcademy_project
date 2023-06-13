1587daaebd22c596f93c893a20d9224976f952db
Egor251 <egorpyshny@gmail.com>
2023-06-13 15:13:50 +0300

    edir ci/cd

--- bbsh_api/tests/test_api.py
+++ bbsh_api/tests/test_api.py
@@ -1,4 +1,5 @@
 import json
+import sqlite3
 import uuid
 
 from httpx import Client
@@ -21,56 +22,70 @@ def check_uuid(uuid_str):
 
 
 def test_user():
-    with Client() as client:
-        data = {"username": "testuser", "password": "testpassword", "phone": "00000000"}
-        resp = client.put(url=f"{base_url}/registration/", json=data)
-        uuid_str = resp.content.decode().replace('"', "")
-        assert check_uuid(uuid_str)
-        assert resp.status_code == 201
-        resp = client.get(url=f"{base_url}/{uuid_str}/profile/")
-        predicted_resp = json.dumps(
-            {
+    conn = sqlite3.connect("../../db.sqlite3")
+    sql = """
+    INSERT INTO bbsh_api_menu (name, cost, size, ingredients)
+    VALUES (?, ?, ?, ?)
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql, ("testposition", 0, "small", "[]"))
+    conn.commit()
+    try:
+        with Client() as client:
+            data = {
                 "username": "testuser",
+                "password": "testpassword",
                 "phone": "00000000",
-                "id": uuid_str,
-                "total orders": 0,
             }
-        )
-        assert predicted_resp in resp.text
-        assert resp.status_code == 200
-        resp = client.get(url=f"{base_url}/{uuid_str}/basket/")
-        assert "[]" in resp.text
-        assert resp.status_code == 200
-        resp = client.get(url=f"{base_url}/{uuid_str}/history/")
-        assert "[]" in resp.text
-        assert resp.status_code == 200
-        data = {"dish_name": "testposition", "dop_ingredients": []}
-        resp = client.put(url=f"{base_url}/{uuid_str}/basket/add/", json=data)
-        assert "true" in resp.text
-        assert resp.status_code == 200
-        resp = client.get(url=f"{base_url}/{uuid_str}/basket/")
-        resp_data = json.loads(resp.text)
-        dish_id = resp_data[0]["id"]
-        predicted_resp = json.dumps(
-            [
+            resp = client.put(url=f"{base_url}/registration/", json=data)
+            uuid_str = resp.content.decode().replace('"', "")
+            assert check_uuid(uuid_str)
+            assert resp.status_code == 201
+            resp = client.get(url=f"{base_url}/{uuid_str}/profile/")
+            predicted_resp = json.dumps(
                 {
-                    "name": "testposition",
-                    "cost": 0,
-                    "size": "small",
-                    "dop_ingredients": "[]",
-                    "id": dish_id,
-                    "user_id": uuid_str,
+                    "username": "testuser",
+                    "phone": "00000000",
+                    "id": uuid_str,
+                    "total orders": 0,
                 }
-            ]
-        )
-        assert predicted_resp in resp.text
-        assert resp.status_code == 200
-        # data = json.dumps({
-        #     "order_id": dish_id
-        # })
-        # resp = client.delete(url=f"{base_url}/{uuid_str}/basket/delete/")
-        # assert "true" in resp.text
-        # assert resp.status_code == 200
-        resp = client.delete(url=f"{base_url}/{uuid_str}/profile/delete/")
-        assert "true" in resp.text
-        assert resp.status_code == 200
+            )
+            assert predicted_resp in resp.text
+            assert resp.status_code == 200
+            resp = client.get(url=f"{base_url}/{uuid_str}/basket/")
+            assert "[]" in resp.text
+            assert resp.status_code == 200
+            resp = client.get(url=f"{base_url}/{uuid_str}/history/")
+            assert "[]" in resp.text
+            assert resp.status_code == 200
+            data = {"dish_name": "testposition", "dop_ingredients": []}
+            resp = client.put(url=f"{base_url}/{uuid_str}/basket/add/", json=data)
+            assert "true" in resp.text
+            assert resp.status_code == 200
+            resp = client.get(url=f"{base_url}/{uuid_str}/basket/")
+            resp_data = json.loads(resp.text)
+            dish_id = resp_data[0]["id"]
+            predicted_resp = json.dumps(
+                [
+                    {
+                        "name": "testposition",
+                        "cost": 0,
+                        "size": "small",
+                        "dop_ingredients": "[]",
+                        "id": dish_id,
+                        "user_id": uuid_str,
+                    }
+                ]
+            )
+            assert predicted_resp in resp.text
+            assert resp.status_code == 200
+            resp = client.delete(url=f"{base_url}/{uuid_str}/profile/delete/")
+            assert "true" in resp.text
+            assert resp.status_code == 200
+    finally:
+        sql = "DELETE FROM bbsh_api_menu WHERE name = 'testposition'"
+        cursor.execute(sql)
+        conn.commit()
+        sql = "DELETE FROM bbsh_api_myuser WHERE username = 'testuser'"
+        cursor.execute(sql)
+        conn.commit()
